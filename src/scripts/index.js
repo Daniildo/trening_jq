a4e61810466177151c105c5267293513fc9aeea5
DaniilTitovsky <daniil.titovsk@mail.ru>
2023-07-30 19:19:08 +0300

    Clen up

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -50,26 +50,5 @@ $(document).ready(function(){
     });
 
 
-    $("body").on("click", ".tabs-header > a", function (event) {
-        event.preventDefault(); // отключаем переход по ссылке
-        const tabIndex = $(this).data("tab-index"); // this - нажатый html тег (ссылка)
-        console.log(`Clicked tab index: ${tabIndex}`);
-        $(this).parent().find("a").removeClass("active"); // удаляем все классы active с .tabs-header>a
-        // parent- родитель, find - найти
-        // $(this).closest(".tabs").find(".tab").fadeOut(600);
-        $(this).closest(".tabs").find(".tab").removeClass("active"); // Удаляем все классы aactive со всех элементов .tab
-        // closest (ближайший) - находит первого родителя с селектором, указанном в первом аргементе (.tabs)
-
-        $(this).closest(".tabs").find(".tab").eq(tabIndex).addClass("active");
-        // $(this).closest(".tabs").find(".tab").eq(tabIndex).fadeIn(600);
-        $(this).addClass("active");
-    });
-
-    $("body").on("click", ".accordion__item__name", function (e) {
-        e.preventDefault();
-        // $(this).closest(".accordion").find(".accordion__item").removeClass("active");
-        // $(this).closest(".accordion__item").addClass("active");
-        $(this).closest(".accordion__item").toggleClass("active");
-    });
 });
 // const, let
